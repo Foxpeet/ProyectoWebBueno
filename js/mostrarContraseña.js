bc8679eb6e5b28187cdbd8e82a0e12debcd87197
Foxpeet <diegobal007@gmail.com>
2022-05-11 19:45:13 +0200

    responsive ojo de mostrar contraseña del login

--- js/mostrarContraseña.js
+++ js/mostrarContraseña.js
@@ -8,10 +8,10 @@ function mostrarContrasena() {
     }
     if (estado == "no") {
         document.getElementById("ojo").style.display = 'none';
-        document.getElementById("ojoTachado").style.display = 'block';
+        document.getElementById("ojoTachado").style.display = 'inline';
         estado="si"
     } else {
-        document.getElementById("ojo").style.display = 'block';
+        document.getElementById("ojo").style.display = 'inline';
         document.getElementById("ojoTachado").style.display = 'none';
         estado="no"
     }
