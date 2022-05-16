c5c10369094965bb1233c8c539dc0e55a4339802
Masterboy272 <90767008+Masterboy272@users.noreply.github.com>
2022-05-16 17:50:00 +0200

    Update login.php

--- src/api/v1.0/login.php
+++ src/api/v1.0/login.php
@@ -2,21 +2,24 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$dbname = "mapas";
+$dbname = "usuarios";
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if(!$conn){
     die("No hay conexion:" .mysqli_connect_error());
 }
 $nombre = $_POST["login"];
 $pass = $_POST["passw"];
-    $query = mysqli_query($conn, "SELECT * FROM usuarios WHERE nombre = '".$nombre."' and password ='".$pass."'");
+    $query = mysqli_query($conn, "SELECT * FROM login WHERE usuario = '".$nombre."' and password ='".$pass."'");
     $nr = mysqli_num_rows($query);
     if($nr==1){
-        if($nombre == "admin"){
+        if($nombre == "admin" && $pass == "1234"){
             echo "<script>window.location='../../vistaDeAdmin.html'; alert('Bienvenido administrador')</script>";
         }
-        else if($nombre != "admin"){
-            echo "<script>window.location='../../userPage.html'; alert('Bienvenido $nombre')</script>";
+        else if($nombre == "user1" && $pass == "1234"){
+            echo "<script>window.location='../../userPage1.html?parcela=1'; alert('Bienvenido $nombre')</script>";
+        }
+        else if($nombre == "user2" && $pass == "1234"){
+            echo "<script>window.location='../../userPage2.html?usuario=2'; alert('Bienvenido $nombre')</script>";
         }
     }
     else{
